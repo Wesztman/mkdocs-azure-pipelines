ac5e8f6c801ec3a5ebc7aa24a574383a3dbfd7c0
Carl Westman <54413402+Wesztman@users.noreply.github.com>
2023-09-06 12:43:20 +0000

    Added error checks and code section

--- ado-pipe-to-md.py
+++ ado-pipe-to-md.py
@@ -3,20 +3,52 @@ import os
 
 def process_pipeline_file(input_file):
     # Define regular expressions to find markers/tags
-    title_pattern = r'#:::title-start:::(.*?)#:::title-end:::'
-    about_pattern = r'#:::about-start:::(.*?)#:::about-end:::'
-    example_pattern = r'#:::example-start:::(.*?)#:::example-end:::'
-    outputs_pattern = r'#:::outputs-start:::(.*?)#:::outputs-end:::'
+    start_tag_pattern = r'#:::(\w+)-start:::'
+    end_tag_pattern = r'#:::(\w+)-end:::'
 
     # Read the input pipeline file
-    with open(input_file, 'r') as f:
+    with open(input_file, 'r', encoding='utf-8') as f:
         pipeline_content = f.read()
 
+    # Check for allowed tags
+    allowed_tags = ['title', 'about', 'example', 'outputs', 'code']
+    found_tags = re.findall(r'#:::(\w+)-start:::', pipeline_content)
+
+    for found_tag in found_tags:
+        if found_tag not in allowed_tags:
+            print(f"Found misspelled start tag: #:::{found_tag}-start:::")
+            print("Allowed tags are:", ', '.join(allowed_tags))
+            return
+
+    # Check for end tags
+    found_end_tags = re.findall(end_tag_pattern, pipeline_content)
+
+    for found_end_tag in found_end_tags:
+        if found_end_tag not in allowed_tags:
+            print(f"Found misspelled end tag: #:::{found_end_tag}-end:::")
+            print("Allowed tags are:", ', '.join(allowed_tags))
+            return
+
+    # Check for tag mismatches
+    start_tags = re.findall(start_tag_pattern, pipeline_content)
+    end_tags = re.findall(end_tag_pattern, pipeline_content)
+    if len(start_tags) != len(end_tags):
+        print("Tag mismatch error: Number of start tags does not match the number of end tags.")
+        print(f"Start tags: {start_tags}")
+        print(f"End tags: {end_tags}")
+        return
+
+    for start_tag, end_tag in zip(start_tags, end_tags):
+        if start_tag != end_tag:
+            print(f"Tag mismatch error: Start tag #{start_tag} does not match end tag #{end_tag}.")
+            return
+
     # Extract content between markers and remove leading '#' characters
-    title = re.search(title_pattern, pipeline_content, re.DOTALL)
-    about = re.search(about_pattern, pipeline_content, re.DOTALL)
-    example = re.search(example_pattern, pipeline_content, re.DOTALL)
-    outputs = re.search(outputs_pattern, pipeline_content, re.DOTALL)
+    title = re.search(r'#:::title-start:::(.*?)#:::title-end:::', pipeline_content, re.DOTALL)
+    about = re.search(r'#:::about-start:::(.*?)#:::about-end:::', pipeline_content, re.DOTALL)
+    example = re.search(r'#:::example-start:::(.*?)#:::example-end:::', pipeline_content, re.DOTALL)
+    outputs = re.search(r'#:::outputs-start:::(.*?)#:::outputs-end:::', pipeline_content, re.DOTALL)
+    code = re.search(r'#:::code-start:::(.*?)#:::code-end:::', pipeline_content, re.DOTALL)
 
     # Create Markdown documentation
     markdown_content = ''
@@ -39,6 +71,13 @@ def process_pipeline_file(input_file):
         example_text = re.sub(r'^# ', '', example_text, flags=re.MULTILINE)
         markdown_content += '## Example\n\n```yaml\n'
         markdown_content += example_text + '\n```\n\n'
+
+    if code:
+        # Remove leading '#' characters from the code content
+        code_text = code.group(1).strip()
+        code_text = re.sub(r'^# ', '', code_text, flags=re.MULTILINE)
+        markdown_content += '## Code\n\n```yaml\n'
+        markdown_content += code_text + '\n```\n\n'
 
     if outputs:
         # Remove leading '#' characters from the outputs content
