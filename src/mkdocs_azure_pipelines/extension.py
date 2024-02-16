bc7caf5de8297760314f801d781d55341178c3b8
Carl Westman <carl.westman@ctek.com>
2024-02-16 15:42:06 +0000

    Add link to mkdocs-click extension

--- src/mkdocs_azure_pipelines/extension.py
+++ src/mkdocs_azure_pipelines/extension.py
@@ -2,3 +2,6 @@ class MkDocsAzurePipelinesException(Exception):
     """
     Generic exception class for mkdocs-azure-pipelines errors.
     """
+
+
+# See https://github.com/mkdocs/mkdocs-click/blob/master/mkdocs_click/_extension.py
